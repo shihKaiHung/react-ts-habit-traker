d22a0c71a1761e71df5a3c9209f9b75a6c3da7d3
Shihkai.Hong <shihkai.hong@awesomegaming.tw>
2019-07-23 17:59:51 +0800

    feat: input component

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Create = () => {
+  const [name, setName] = useState("");
+
   return (
-    <div>
-      test
-    </div>
-  )
+    <InputGroup>
+      <InputGroup>
+        <input />
+        <label>Habit Name</label>
+      </InputGroup>
+    </InputGroup>
+  );
 };
 
 export default Create;
+
+const Input = styled.input``;
+
+const InputGroup = styled.div`
+  position: relative;
+  padding-top: 20px;
+  margin-top: 20px;
+  &:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  input {
+    border: none;
+    border-bottom: solid 1px #ccc;
+    width: 100%;
+    padding: 10px 0;
+    transition: border-color 0.2s ease, bow-shadow 0.2s ease;
+    &:focus {
+      outline: none;
+      border-color: #20cfb8;
+      box-shadow: 0 1px 0 0 #20cfb8;
+    }
+  }
+  input:focus + label {
+    color: #20cfb8;
+  }
+  label {
+    position: absolute;
+    top: 30px;
+    left: 0;
+    color: #aaa;
+    font-weight: 300;
+    transition: transform 0.2s ease, color 0.2s ease;
+    transform: translateY(-30px);
+    &:hover {
+      cursor: text;
+    }
+  }
+`;
